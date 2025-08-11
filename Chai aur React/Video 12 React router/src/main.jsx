04dc4c440dd7b3b1aeaece99bdb39bf0e664da68
Bhavay Nagpal <bhavaynagpal000@gmail.com>
2025-08-11 19:04:39 +0530

    correction

--- Chai aur React/Video 12 React router/src/main.jsx
+++ Chai aur React/Video 12 React router/src/main.jsx
@@ -29,6 +29,7 @@ import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 //     ]
 //   }   // Layout is shared, but the middle content depends on the path.
 // ])
+
 /*
 User visits /about
 ↓
@@ -43,7 +44,7 @@ Layout component is rendered
 // another way of creating routes
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='' element={<Layout />}>
+    <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />} />
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
